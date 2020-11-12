7b81fd10e30c0c0c0e3f3ba133bbc662171e5fc7
Ambika Eshwar <ambika.eshwar@rutgers.edu>
2020-11-11 23:59:50 -0500

    Refactored countercog and fixed filename bug

--- cogs/countercog.py
+++ cogs/countercog.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import json
-from .utils import create_ine
+from .utils import create_ine, get_data
 
 
 class CounterCog(commands.Cog):
@@ -13,14 +13,8 @@ class CounterCog(commands.Cog):
     @commands.has_role("Bot Commander")
     async def setcounter(self, ctx, name, count="0"):
         """Sets a counter with the specified name and optional starting count, default is zero"""
-
-        with open("data/counters.json", "r") as f:
-            counters = json.load(f)
-
-        counters[name] = count
-
-        with open("data/counters.json", "w") as f:
-            json.dump(counters, f)
+        with get_data("counters") as counters:
+            counters[name] = count
 
         await ctx.send(f"Counter {name} updated to {count}")
 
@@ -28,29 +22,20 @@ class CounterCog(commands.Cog):
     @commands.has_role("Bot Commander")
     async def removecounter(self, ctx, name):
         """Removes a counter given counter name"""
-
-        with open("data/counters.json", "r") as f:
-            counters = json.load(f)
-
-        try:
-            counters.pop(name)
-        except:
-            await ctx.send("No counter with that name!")
-            return
-
-        with open("data/counters.json", "w") as f:
-            json.dump(counters, f)
+        with get_data("counters") as counters:
+            try:
+                counters.pop(name)
+            except KeyError:
+                await ctx.send("No counter with that name!")
+                return
 
         await ctx.send(f"There is now no counter for {name}")
 
     @commands.command()
     async def listcounters(self, ctx):
         """Lists counters"""
-
-        with open("data/counters.json", "r") as f:
-            counters = json.load(f)
-
-        desc = "\n".join([k for k in counters.keys()])
+        with get_data("counters", update=False) as counters:
+            desc = "\n".join([k for k in counters.keys()])
 
         embed = discord.Embed(title="Counters", description=desc, color=0xFF0000)
 
@@ -60,18 +45,12 @@ class CounterCog(commands.Cog):
     @commands.has_role("Bot Commander")
     async def increment(self, ctx, name, amt="1"):
         """Increments specified counter by a specified amount, default is 1"""
-
-        with open("data/counters.json", "r") as f:
-            counters = json.load(f)
-
-        try:
-            counters[name] = str(int(counters[name]) + int(amt))
-        except:
-            await ctx.send("No counter with that name!")
-            return
-
-        with open("data/counters.json", "w") as f:
-            json.dump(counters, f)
+        with get_data("counters") as counters:
+            try:
+                counters[name] = str(int(counters[name]) + int(amt))
+            except KeyError:
+                await ctx.send("No counter with that name!")
+                return
 
         await ctx.send(
             f"Counter {name} updated from {int(counters[name]) - int(amt)} to {counters[name]}"
@@ -81,18 +60,12 @@ class CounterCog(commands.Cog):
     @commands.has_role("Bot Commander")
     async def decrement(self, ctx, name, amt="1"):
         """Decrements specified counter by a specified amount, default is 1"""
-
-        with open("data/counters.json", "r") as f:
-            counters = json.load(f)
-
-        try:
-            counters[name] = str(int(counters[name]) - int(amt))
-        except:
-            await ctx.send("No counter with that name!")
-            return
-
-        with open("data/counters.json", "w") as f:
-            json.dump(counters, f)
+        with get_data("counters") as counters:
+            try:
+                counters[name] = str(int(counters[name]) - int(amt))
+            except KeyError:
+                await ctx.send("No counter with that name!")
+                return
 
         await ctx.send(
             f"Counter {name} updated from {int(counters[name]) + int(amt)} to {counters[name]}"
@@ -101,14 +74,11 @@ class CounterCog(commands.Cog):
     @commands.command()
     async def counter(self, ctx, name):
         """Checks the value of a counter"""
-
-        with open("data/counters.json", "r") as f:
-            counters = json.load(f)
-
-        try:
-            await ctx.send(counters[name])
-        except:
-            await ctx.send("There is no counter with that name!")
+        with get_data("counters") as counters:
+            try:
+                await ctx.send(counters[name])
+            except KeyError:
+                await ctx.send("There is no counter with that name!")
 
 
 def setup(bot):
